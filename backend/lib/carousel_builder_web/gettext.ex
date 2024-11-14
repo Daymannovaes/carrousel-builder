42156a25f0a1c8ba1004c7565da81dd46c6ca555
Antônio Augusto Duarte <aadm.aquino@gmail.com>
2024-11-14 19:38:13 -0300

    Solve gettext warning

--- backend/lib/carousel_builder_web/gettext.ex
+++ backend/lib/carousel_builder_web/gettext.ex
@@ -20,5 +20,5 @@ defmodule CarouselBuilderWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :carousel_builder
+  use Gettext.Backend, otp_app: :carousel_builder
 end
